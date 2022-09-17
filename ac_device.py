b0a259a9014554b9980263ba584b2f1a9d0951e0
Ubuntu <ubuntu@ip-172-31-38-65.ap-south-1.compute.internal>
2022-09-17 13:49:39 +0000

    refactor for inheritence from abstract base

--- ac_device.py
+++ ac_device.py
@@ -1,31 +1,20 @@
 
 import json
 import paho.mqtt.client as mqtt
+from device import Device
 
 
-HOST = "localhost"
-PORT = 1883
-    
-class ACDevice():
+class ACDevice(Device):
     
     _MIN_TEMP = 18  
     _MAX_TEMP = 32  
 
     def __init__(self, device_id, room):
         
-        self._device_id = device_id
-        self._room_type = room
         self._temperature = 22
         self._device_type = "AC"
-        self._device_registration_flag = False
-        self.client = mqtt.Client(self._device_id)  
-        self.client.on_connect = self._on_connect  
-        self.client.on_message = self._on_message  
-        self.client.on_disconnect = self._on_disconnect  
-        self.client.connect(HOST, PORT, keepalive=60)  
-        self.client.loop_start()  
+        super().__init__(device_id, room)
         self._register_device(self._device_id, self._room_type, self._device_type)
-        self._switch_status = "OFF"
 
     # calling registration method to register the device
     def _register_device(self, device_id, room_type, device_type):
